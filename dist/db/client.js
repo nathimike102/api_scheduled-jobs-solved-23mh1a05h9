4f8138ac8a1cd5499756e129f4c8603b47dd1895
ghost <ghost@example.com>
2026-03-09 07:34:12 +0530

    chore: ignore dist directory

--- dist/db/client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-// Instantiate a single PrismaClient globally to avoid connection exhaustion in dev
-const prisma = new client_1.PrismaClient();
-exports.default = prisma;
